c7b4403900f9a85e8d73960e0f1b34aba236ae90
Baaartosz <bartosz_p@hotmail.com>
2022-04-25 10:40:01 +0100

    Add project files.

---
+++ imgui_template.cpp
@@ -0,0 +1,12 @@
+﻿// imgui_template.cpp : Defines the entry point for the application.
+//
+
+#include "imgui_template.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
